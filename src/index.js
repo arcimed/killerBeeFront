117c1ccec37988437135a2dcf62fa6fcdca709d0
arcimed <julien.fallempin@orange.fr>
2021-12-17 13:44:45 +0100

    suppresion cookieprovider

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CookiesProvider>
       <App />
-    </CookiesProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
